f152e67e6dce7d3578850022cecd2f56381873d1
Jonathan Im <imjonathan.work@gmail.com>
2023-09-26 19:50:50 -0700

    working on switch case

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 let startingValue = '0'
+let total = 0
+let firstOperator;
 const screenValue = document.querySelector('#screenValue')
 
 const handleKeyPress = (value) => {
@@ -18,10 +20,14 @@ const handleSymbol = (value)=> {
 
     switch(value) {
         case 'C':
-            screenValue.innerText = 0
+            screenValue.innerText = '0'
             break;
         case '←':
-         
+            if(startingValue.length === 1){
+                startingValue = '0'
+            }else {
+                startingValue = startingValue.substring(0, startingValue.length -1)
+            }
         case '÷':
             break;
 
@@ -30,18 +36,50 @@ const handleSymbol = (value)=> {
         case '−':
             break;
         case '+':
+           
             break;
-        case '-':
+        case '=':
             break;
     }
 }
 
+const handleCalculations = (value)=> {
+    if(startingValue == '0'){
+        return;
+    }
+    const changeToNumber = parseInt(value)
+    if(total == 0){
+        total = changeToNumber
+    }else {
+        doMath(changeToNumber)
+    }
+    firstValue = value;
+    startingValue = '0'
+}
+
+const doMath=(IntValue)=>{
+    if(firstValue == '+'){
+        total+= changeToNumber
+    }
+    else if(firstValue == '−'){
+        total-= changeToNumber
+    }
+    else if(firstValue == '×'){
+        total*= changeToNumber
+    }
+    else if(firstValue == '÷'){
+        total/= changeToNumber
+    }
+   
+}
+
 const handleNumber =(value)=> {
-//     const convertToNumber = parseInt(value)
-//     if(startingValue == '0'){
-//         startingValue = value
-//     ""
-// working on logic
+    
+    if(startingValue === '0'){
+        startingValue = value;
+    }else {
+        startingValue+= value;
+    }
 }
 
 
@@ -53,7 +91,6 @@ function init() {
     const allkeybuttons = document.querySelectorAll('.cal-buttons')
     allkeybuttons.forEach(button => button.addEventListener("click", (e)=> handleKeyPress(e.target.innerText)))
 
-    
 }
 
 
